f7e763bfecb1615e1fa15187bd1bb35bc428f3e8
MEHAK SHARMA <144684854+MehakSharma444@users.noreply.github.com>
2025-08-11 16:45:01 +0530

    db_handler.py

--- db_handler.py
+++ db_handler.py
@@ -2,29 +2,31 @@ import sqlite3
 import pandas as pd
 from datetime import datetime
 
+DB_NAME = "carbon_emission_data.db"
+
 def create_database():
-    conn = sqlite3.connect("carbon_emission_dataa.db")
+    conn = sqlite3.connect(DB_NAME)
     cursor = conn.cursor()
     cursor.execute("""
-            CREATE TABLE IF NOT EXISTS user_emissions (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                name TEXT,
-                country TEXT,
-                age INTEGER,
-                gender TEXT,
-                transport REAL,
-                electricity REAL,
-                diet REAL,
-                waste REAL,
-                total REAL,
-                timestamp TEXT
-            )
-        """)
+        CREATE TABLE IF NOT EXISTS user_emissions (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            country TEXT,
+            age INTEGER,
+            gender TEXT,
+            transport REAL,
+            electricity REAL,
+            diet REAL,
+            waste REAL,
+            total REAL,
+            timestamp TEXT
+        )
+    """)
     conn.commit()
     conn.close()
 
 def save_user_data(name, country, age, gender, transport, electricity, diet, waste, total):
-    conn = sqlite3.connect("carbon_emission_dataa.db")
+    conn = sqlite3.connect(DB_NAME)
     cursor = conn.cursor()
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     cursor.execute("""
@@ -35,8 +37,9 @@ def save_user_data(name, country, age, gender, transport, electricity, diet, was
     conn.close()
 
 def get_recent_data(limit=10):
-    conn = sqlite3.connect("carbon_emission_dataa.db")
-    df = pd.read_sql_query(f"SELECT * FROM user_emissions ORDER BY timestamp DESC LIMIT {limit}", conn)
+    conn = sqlite3.connect(DB_NAME)
+    df = pd.read_sql_query(
+        f"SELECT * FROM user_emissions ORDER BY timestamp DESC LIMIT {limit}", conn
+    )
     conn.close()
     return df
-
